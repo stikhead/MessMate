21a69c87c7ce1215727be9024891f5a9dfc9c599
Anirudh Bansal <anirudhbansal005@gmail.com>
2026-03-02 04:12:12 +0530

    fix base url

--- mess-web/src/lib/api.ts
+++ mess-web/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-  baseURL: "process.env.NEXT_PUBLIC_API_URL",
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": true
