76ae270242d8cdf515d88f93ef32538be5d97296
Anirudh Bansal <anirudhbansal005@gmail.com>
2026-04-02 12:12:42 +0530

    fix

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -2,6 +2,7 @@ import { User } from "../models/users.models.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import { sendVerificationEmail } from "../services/email.service.js";
 
 const isOtpAvailableAndGenerateOtp = async (user) => {
     const currentTime = Date.now();
